041feed5046a004ff2e1ceb136f6606af5705667
josukutty <josukuttyulahannan@gmail.com>
2022-07-20 17:48:25 +0530

    added configuration file,updated main.py

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 #TODO import necessary packages
+import yaml
+import requests
+import json
+import time
+import asyncio
+from yaml.loader import SafeLoader
+from collections import defaultdict
 
-def get_data_from_prometheus(metric_name, start_time, end_time, url):
-    data_points = []
+
+
+def prometheus(query):
+    result = json.loads(requests.get(query).text)
+    value = result['data']['result']
+    return value
+
+def get_data_from_prometheus(metric_name,prom_query, start_time, end_time, url):
+    data_points = defaultdict(list)
+    for i in range(len(metric_name)):
+        if metric_name[i]:
+            query = url+'/api/v1/query_range?query='+prom_query[i]+'&start='+start_time[i]+'&end='+end_time[i]+'&step=30s'
+            #print(query)
+            result = prometheus(query)
+            for elements in result:
+                values = elements['values']
+                for element in values:
+                    data_points[metric_name[i]].append(element[1])
     return data_points
 
 def train_model():
     pass
 
-async def predict_datapoints(metric['forecast_every'], metric['name']..):
+async def predict_datapoints(forecast_every,metric_name):
      while True:
         #TODO If trained model available, else break
         #Get the past data points and pass that to the model for prediction
@@ -24,9 +47,27 @@ if __name__ == "__main__":
     #Check datastore type, if prometheus get it from get_data_from_promethues(metric_name, start_time, end_time, url)
     #Check if there's a trained model available for the given metric, else do the training also check the retrain flag in the config of each metric
     #Give predictions back to the prometheus if trained model is available locally
-    data_for_training = get_data_from_prometheus(metric_name, start_time, end_time, url)
-    for metric in metrics:
-        predict_datapoints(metric['forecast_every'], metric['name']..)
+    with open('./config.yaml') as f:
+        data = yaml.load(f, Loader=SafeLoader)
+    #print(data)
+
+    metric_dict = defaultdict(list)
+    for elements in data['metrics']:
+        for element in elements:
+            metric_dict[element].append(elements[element])
+
+    metric_name = metric_dict['name']
+    start_time = metric_dict['start_time']
+    end_time = metric_dict['end_time']
+    url = data['prometheus_url']
+    prom_query = metric_dict['query']
+    forecast_every = metric_dict['forecast_every']
+
+    data_for_training = get_data_from_prometheus(metric_name,prom_query, start_time, end_time, url)
+    print(data_for_training)
+
+    for metric in metric_dict:
+        asyncio.run(predict_datapoints(forecast_every, metric_name))
     while True:
         #get status of the async functions and restart failed ones
         time.sleep(30)
