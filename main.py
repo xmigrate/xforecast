26a83e47c5ebbcd96265042e07a720eaa4fc6f2b
WeepingClown13 <ananthucv5@gmail.com>
2022-09-13 09:39:48 +0000

    updated docstrings and changed query variable name

--- main.py
+++ main.py
@@ -21,15 +21,16 @@ async def main():
         metric_list.append(metric)
     await forecast(metric_list)
         
-async def predict_every(metric_name,data_store,start_time,end_time,prom_query,write_back_metric,forecast_every,forecast_basedon,model):
+async def predict_every(metric_name,data_store,start_time,end_time,db_query,write_back_metric,forecast_every,forecast_basedon,model):
     """Calls fit_and_predict function at the required intervals
 
     Parameters
     ----------
     metric_name : metric name in prometheus
     start_time : start time for the prometheus query
+    data_store : dictionary containing details of the database used for query
     end_time : end time for the prometheus query
-    prom_query = Prometheus query
+    db_query = database query
     write_back_metric = name of the predicted/written metric
     forecast_every: at what interval the app does the predictions
     forecast_basedon: forecast based on past how many data points
@@ -59,10 +60,10 @@ async def predict_every(metric_name,data_store,start_time,end_time,prom_query,wr
                     start_time = end_time - timedelta(minutes=t)
                     start_time = start_time.strftime('%Y-%m-%d %H:%M:%S')
                     end_time = end_time.strftime('%Y-%m-%d %H:%M:%S')
-                await fit_and_predict(metric_name,data_store,start_time,end_time,prom_query,write_back_metric,model,prev_stime,prev_etime,periods=periods,frequency='60s',old_model_loc=old_model_loc)
+                await fit_and_predict(metric_name,data_store,start_time,end_time,db_query,write_back_metric,model,prev_stime,prev_etime,periods=periods,frequency='60s',old_model_loc=old_model_loc)
             else:
                 #print("og")
-                await fit_and_predict(metric_name,data_store,start_time,end_time,prom_query,write_back_metric,model,prev_stime,prev_etime,periods=periods,frequency='60s',old_model_loc=None)
+                await fit_and_predict(metric_name,data_store,start_time,end_time,db_query,write_back_metric,model,prev_stime,prev_etime,periods=periods,frequency='60s',old_model_loc=None)
             n+=1
             await asyncio.sleep((forecast_every))
 
